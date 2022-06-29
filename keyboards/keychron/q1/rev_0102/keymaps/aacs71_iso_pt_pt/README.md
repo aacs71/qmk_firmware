7d6848fe01e0817a1bca98d6bd5c548c075bf596
aacs71 <antonio.costa.salgado@gmail.com>
2022-06-29 23:45:35 +0100

    Keychron Q1 PT ISO Layout and right columns redefined

---
+++ keyboards/keychron/q1/rev_0102/keymaps/aacs71_iso_pt_pt/README.md
@@ -0,0 +1,16 @@
+# Keychron Q1 keymap ISO Portuguese - Portugal
+
+This ISO layout got heavily inspired by kubahorak work (rev_0102).
+
+Features:
+- The rightest key column is: *PgUp, PgDn, Home, End* which differs from the default.
+- RGB lighting turns off when the computer sleeps
+- Caps Lock and alpha keys turn red to indicate when Caps Lock is on
+- When the Fn layer is active, RGB lighting turns off for keys that are not assigned
+
+## Changelog
+
+### 2022-06-29 - 1.0.0
+
+- Initial release
+
---
+++ keyboards/keychron/q1/rev_0102/keymaps/aacs71_iso_pt_pt/mac_pt_iso_overrides.h
@@ -0,0 +1,35 @@
+/* Copyright 2022 António Salgado
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#pragma once
+
+#include "keymap.h"
+
+// MAC OVERRIDES
+#define MAC_PT_BSLS KC_NUHS     // (backslash)
+#define MAC_PT_QUOT KC_MINS     // '
+#define MAC_PT_LDAQ A(PT_X)     // «
+#define MAC_PT_PLUS KC_EQL      // +
+#define MAC_PT_ACUT KC_RBRC     // ´ (dead)
+#define MAC_PT_CCED KC_SCLN     // Ç
+#define MAC_PT_MORD KC_LBRC     // º
+#define MAC_PT_TILD KC_QUOT     // ~ (dead)
+#define MAC_PT_LABK KC_NUBS     // <
+#define MAC_PT_COMM KC_COMM     // ,
+#define MAC_PT_DOT  KC_DOT      // .
+#define MAC_PT_MINS KC_SLSH     // -
+
