e8d49c67fb6d0c0c2fab21138a93c7f6285cd60f
aacs71 <antonio.costa.salgado@gmail.com>
2022-06-30 01:26:08 +0100

    Added Insert to Layer 2 swapping Del

--- keyboards/keychron/q1/rev_0102/keymaps/aacs71_iso_pt_pt/keymap.c
+++ keyboards/keychron/q1/rev_0102/keymaps/aacs71_iso_pt_pt/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_LCTL,        KC_LALT,       KC_LGUI,   KC_SPC,   KC_RGUI,   MO(1),     KC_RCTL,   KC_LEFT,   KC_DOWN,   KC_RGHT
             ),
 	[MAC_FN] = LAYOUT_iso_83(
-            KC_ESC,    KC_BRID,    KC_BRIU,   KC_TRNS,   KC_TRNS,   RGB_VAD,   RGB_VAI,   KC_MPRV,   KC_MPLY,   KC_MNXT,   KC_MUTE,   KC_VOLD,   KC_VOLU,   KC_TRNS,   KC_TRNS,
+            KC_ESC,    KC_BRID,    KC_BRIU,   KC_TRNS,   KC_TRNS,   RGB_VAD,   RGB_VAI,   KC_MPRV,   KC_MPLY,   KC_MNXT,   KC_MUTE,   KC_VOLD,   KC_VOLU,   KC_INS,   KC_TRNS,
             KC_TRNS,   KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_P7,     KC_P8,     KC_P9,     KC_PSLS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
             RGB_TOG,   RGB_MOD,    RGB_VAI,   RGB_HUI,   RGB_SAI,   RGB_SPI,   KC_TRNS,   KC_P4,     KC_P5,     KC_P6,     KC_PAST,   KC_TRNS,   KC_TRNS,   KC_TRNS,
             KC_TRNS,   RGB_RMOD,   RGB_VAD,   RGB_HUD,   RGB_SAD,   RGB_SPD,   KC_TRNS,   KC_P1,     KC_P2,     KC_P3,     KC_PMNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_LCTL,   KC_LGUI,   KC_LALT,   KC_SPC,   KC_RALT,   MO(3),   KC_RCTL,   KC_LEFT,   KC_DOWN,   KC_RGHT
             ),
 	[WIN_FN] = LAYOUT_iso_83(
-            KC_TRNS,   KC_BRID,    KC_BRIU,   LGUI(KC_TAB),   LGUI(KC_E),   RGB_VAD,   RGB_VAI,   KC_MPRV,   KC_MPLY,   KC_MNXT,   KC_MUTE,   KC_VOLD,   KC_VOLU,   KC_TRNS,   KC_TRNS,
+            KC_TRNS,   KC_BRID,    KC_BRIU,   LGUI(KC_TAB),   LGUI(KC_E),   RGB_VAD,   RGB_VAI,   KC_MPRV,   KC_MPLY,   KC_MNXT,   KC_MUTE,   KC_VOLD,   KC_VOLU,   KC_INS,   KC_TRNS,
             KC_TRNS,   KC_TRNS,    KC_TRNS,   KC_TRNS,        KC_TRNS,      KC_TRNS,   KC_TRNS,   KC_P7,     KC_P8,     KC_P9,     KC_PSLS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
             RGB_TOG,   RGB_MOD,    RGB_VAI,   RGB_HUI,        RGB_SAI,      RGB_SPI,   KC_TRNS,   KC_P4,     KC_P5,     KC_P6,     KC_PAST,   KC_TRNS,   KC_TRNS,   KC_TRNS,
             KC_TRNS,   RGB_RMOD,   RGB_VAD,   RGB_HUD,        RGB_SAD,      RGB_SPD,   KC_TRNS,   KC_P1,     KC_P2,     KC_P3,     KC_PMNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
